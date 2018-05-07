4339318ee5a15f7c16137a16e64488c04a4080c1
Le tu <levantu.13139@gmail.com>
2018-05-08 01:17:16 +0700

    update Invoicemain.js

--- E-Invoice_System/src/main/webapp/resources/assets/js/InvoicesMain.js
+++ E-Invoice_System/src/main/webapp/resources/assets/js/InvoicesMain.js
@@ -1,37 +1,35 @@
 var invoicesdata;
-var codein,int,indtime;
-$(document).ready(function(){
 
-	$.ajax({url: "http://localhost:8080/E-Invoice_System/getInvoiceFromUser", success: function(result){
-			invoicesdata = result;
-			listDataArray(invoicesdata);
-			
-    }});
-	function listDataArray(data){
-		$('.container .P-List-invoice .list-group').ready(function(){
-    		for(var i = 0; i < data.length; i++){
-    			codein = data[i].invoiceNo;
-    			inty = data[i].type;
-    			indtime = data[i].dateTime;
-    			var markup = "<li class='list-group-item d-flex justify-content-between align-items-center borderless'>"+"<span class='i-circle align-self-center col-sm-4 col-lg-4 col-xs-4' id='detail'>"
-    			+codein[0] 
-    			+"</span><span class='align-self-center col-xs-9 col-lg-9 P-info-invoice' valign='center'><b style='font-size:20px'>"
-    			+ codein
-    			+"</b></br>"
-    			+ inty
-    			+ " | " +  indtime
-    			+"</span>"
-    			+"<span class='align-self-center col-xs-1 col-lg-1' valign='center'>"
-    			+"<span class='glyphicon glyphicon-remove delete'/>"
-    			+"</br>"
-    			+"<span class='glyphicon glyphicon-edit edit'/>"
-    			+"</span>" 
-    			+"</li>";
-    			$(".container .P-List-invoice .list-group").append(markup);
-    		}
-    	});
-	}
-	function detailInvoice(detailarray){
-		
-	}
+function getSolutionjson() {
+    var str;
+    $.ajax({
+        url: "http://localhost:8080/E-Invoice_System/getInvoiceFromUser",
+        type: 'GET',
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        processData: true,
+        success: function (data) {
+            str = data;
+        },
+        failure: function (data) {
+            alert("Fail " + data);
+        }
+
+    });
+    return str;
+};
+
+
+function listDataArray(data){
+	$('.page-container').append('haha');
+};
+
+
+
+
+$(document).ready(function(){
+	invoicesdata = getSolutionjson();
+	listDataArray(invoicesdata);
+	console.log(invoicesdata);
 });
